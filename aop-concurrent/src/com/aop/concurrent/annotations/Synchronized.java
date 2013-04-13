c501a17cca0dc189000c8b5a13f64fe45bd610da
Michał Nowak <minowak@student.agh.edu.pl>
2013-04-13 12:09:59 +0200

    Synchronized annotation

---
+++ aop-concurrent/src/com/aop/concurrent/annotations/Synchronized.java
@@ -0,0 +1,17 @@
+package com.aop.concurrent.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for synchronizing methods.
+ *
+ * @author nowak
+ */
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Synchronized {
+
+}
