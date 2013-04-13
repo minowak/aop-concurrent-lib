e2dd53151c1ed934beaab3822d48a53a3add9061
Michał Nowak <minowak@student.agh.edu.pl>
2013-04-13 13:27:33 +0200

    Synchronized tests working

--- aop-concurrent-test/src/com/aop/concurrent/test/SynchronizedTest.java
+++ aop-concurrent-test/src/com/aop/concurrent/test/SynchronizedTest.java
@@ -12,22 +12,24 @@ public class SynchronizedTest {
 
 	private final static String EXAMPLE_TEXT2 = "ASDFGHJKL";
 
-	private final static long TIME1 = 500;
+	private final static long TIME1 = 50;
 
-	private final static long TIME2 = 100;
+	private final static long TIME2 = 5;
 
 	/**
 	 * Tests @Synchronized in one class.
 	 */
 	@Test
-	public void testSynchronizedInOneClass() {
+	public void testSynchronizedInOneClass() throws InterruptedException {
 		// given
 		ExampleSynchronized es1 = new ExampleSynchronized(EXAMPLE_TEXT1, TIME1);
 		ExampleSynchronized es2 = new ExampleSynchronized(EXAMPLE_TEXT2, TIME2);
 
 		// when
-		es1.run();
-		es2.run();
+		es1.start();
+		es2.start();
+		es1.join();
+		es2.join();
 
 		// then
 		assertEquals(es1.getCurrentText(), String.format("%s%s", EXAMPLE_TEXT1, EXAMPLE_TEXT2));
